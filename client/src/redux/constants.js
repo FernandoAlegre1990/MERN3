155d21f6a4b13e0c973edba035682ed95d3e0fa5
Fernando Ezequiel Alegre <fernandoalegre31@gmail.com>
2024-10-02 20:48:00 +0200

    Update constants.js

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://mern3-101c.onrender.com";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/uploads";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const USERS_URL = "https://mern3-101c.onrender.com/api/users";
+export const CATEGORY_URL = "https://mern3-101c.onrender.com/api/category";
+export const PRODUCT_URL = "https://mern3-101c.onrender.com/api/products";
+export const UPLOAD_URL = "https://mern3-101c.onrender.com/api/uploads";
+export const ORDERS_URL = "https://mern3-101c.onrender.com/api/orders";
+export const PAYPAL_URL = "https://mern3-101c.onrender.com/api/config/paypal";
