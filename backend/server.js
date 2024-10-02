b73b494d19a47f9c3baff3786c46cdaa48a209da
Fernando Ezequiel Alegre <fernandoalegre31@gmail.com>
2024-10-02 21:28:20 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-// packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"; // Importar cors
+import { fileURLToPath } from 'url'; // Importar para crear __dirname
+
+// Crear __dirname manualmente en ES6
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -21,7 +25,7 @@ connectDB();
 const app = express();
 
 // CORS configuration
-const allowedOrigins = ["https://mern3-client.onrender.com"]; // Lista de orígenes permitidos
+const allowedOrigins = ["https://mern3-client.onrender.com"];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true, // Permitir cookies si las usas
@@ -32,24 +36,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// API Routes
-app.use("/api/users", userRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/uploads", uploadRoutes);
-app.use("/api/orders", orderRoutes);
-
-// Paypal config
-app.get("/api/config/paypal", (req, res) => {
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-});
-
 // Serve uploads folder as static
-app.use("/uploads", cors(), express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Serve static files in production
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
@@ -66,7 +56,6 @@ if (process.env.NODE_ENV === "production") {
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
-
 
 // Start the server
 app.listen(port, () => console.log(`Server running on port: ${port}`));
